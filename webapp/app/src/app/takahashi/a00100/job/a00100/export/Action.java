10ee9806ca7c02b5fa42386a25e618b07cc0912f
NO001\hidek <hideki.nakayama@pleinsoleil.jp>
2021-02-21 14:42:58 +0900

    update

---
+++ webapp/app/src/app/takahashi/a00100/job/a00100/export/Action.java
@@ -0,0 +1,7 @@
+package app.takahashi.a00100.job.a00100.export;
+
+public class Action extends common.app.job.app.action.Action {
+	public Action() {
+		super(new App());
+	}
+}
---
+++ webapp/app/src/app/takahashi/a00100/job/a00100/export/App.java
@@ -0,0 +1,20 @@
+package app.takahashi.a00100.job.a00100.export;
+
+import common.app.job.app.bean.Bean;
+import common.app.job.app.model.Model;
+
+public class App extends common.app.App {
+	public App() {
+		super(new Bean(), new Model());
+	}
+
+	@Override
+	public String getConfigFileName() {
+		return null;
+	}
+
+	@Override
+	public String getDataSourceName() {
+		return null;
+	}
+}
