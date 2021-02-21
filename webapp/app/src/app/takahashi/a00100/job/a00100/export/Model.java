50213fd12c40f68ccf77c0dcfb1a790e1c1b2123
NO001\hidek <hideki.nakayama@pleinsoleil.jp>
2021-02-21 15:20:27 +0900

    update

--- webapp/app/src/app/takahashi/a00100/job/a00100/export/Model.java
+++ webapp/app/src/app/takahashi/a00100/job/a00100/export/Model.java
@@ -1,4 +1,26 @@
 package app.takahashi.a00100.job.a00100.export;
 
+import lombok.experimental.Accessors;
+
+@Accessors(prefix = "m_", chain = false)
 public class Model extends app.takahashi.a00100.job.common.app.Model {
+	static boolean m_executing = false;
+
+	@Override
+	public void execute() throws Exception {
+		if (isExecutable() == true) {
+			try {
+				job();
+			} finally {
+				m_executing = false;
+			}
+		}
+	}
+
+	synchronized boolean isExecutable() {
+		return (m_executing == false ? m_executing = true : m_executing);
+	}
+
+	void job() throws Exception {
+	}
 }
--- webapp/app/src/app/takahashi/app/job/app/a00100/test/Action.java
+++
@@ -1,7 +0,0 @@
-package app.takahashi.app.job.app.a00100.test;
-
-public class Action extends common.app.job.app.action.Action {
-	public Action() {
-		super(new App());
-	}
-}
