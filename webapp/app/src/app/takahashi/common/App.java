e1a5d3ae515969ddf65fe502b2a12b1c522f6b32
NO001\hidek <hideki.nakayama@pleinsoleil.jp>
2021-02-21 14:58:00 +0900

    update

--- webapp/app/src/app/takahashi/common/App.java
+++ webapp/app/src/app/takahashi/common/App.java
@@ -1,7 +1,7 @@
 package app.takahashi.common;
 
-import common.app.job.app.bean.Bean;
-import common.app.job.app.model.Model;
+import common.app.bean.Bean;
+import common.app.model.Model;
 
 public abstract class App extends common.app.App {
 	public App(final Bean bean, final Model model) {
