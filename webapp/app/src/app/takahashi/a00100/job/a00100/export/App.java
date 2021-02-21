97bfbd5ff4a7d704211bb270ff4310ace0af00ad
NO001\hidek <hideki.nakayama@pleinsoleil.jp>
2021-02-21 15:05:01 +0900

    update

--- webapp/app/src/app/takahashi/a00100/job/a00100/export/App.java
+++ webapp/app/src/app/takahashi/a00100/job/a00100/export/App.java
@@ -1,8 +1,5 @@
 package app.takahashi.a00100.job.a00100.export;
 
-import app.takahashi.a00100.job.common.Bean;
-import app.takahashi.a00100.job.common.Model;
-
 public class App extends app.takahashi.a00100.job.common.App {
 	public App() {
 		super(new Bean(), new Model());
---
+++ webapp/app/src/app/takahashi/a00100/job/a00100/export/Model.java
@@ -0,0 +1,4 @@
+package app.takahashi.a00100.job.a00100.export;
+
+public class Model extends app.takahashi.a00100.job.common.Model {
+}
