1dbce12171cf6944ffbcf76a5353552d8c79bb18
NO001\hidek <hideki.nakayama@pleinsoleil.jp>
2021-02-21 15:14:44 +0900

    update

--- webapp/app/src/app/takahashi/a00100/job/common/App.java
+++ webapp/app/src/app/takahashi/a00100/job/common/App.java
@@ -1,17 +1,29 @@
 package app.takahashi.a00100.job.common;
 
+import common.lang.StringUtils;
+import lombok.val;
+
 public class App extends app.takahashi.a00100.App {
+	public static final String DEFAULT_DSN;
+	public static final String CONFIGFILE_NAME;
+
+	static {
+		val names = StringUtils.split(App.class.getPackage().getName(), ".");
+		DEFAULT_DSN = String.format("jdbc/%s/%s/default", names[1], names[2]);
+		CONFIGFILE_NAME = String.format("config-%s-%s-job.xml", names[1], names[2]);
+	}
+
 	public App(final Bean bean, final Model model) {
 		super(bean, model);
 	}
 
 	@Override
 	public String getConfigFileName() {
-		return null;
+		return CONFIGFILE_NAME;
 	}
 
 	@Override
 	public String getDataSourceName() {
-		return null;
+		return DEFAULT_DSN;
 	}
 }
