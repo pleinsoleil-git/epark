79e7237af9875ad0c44882e693c087c5d1f742b0
hidek <hidek@NT001>
2021-02-23 09:25:28 +0900

    update

--- webapp/app/src/app/takahashi/a00100/job/a00100/crawler/job/request/crawler/WebBrowser.java
+++ webapp/app/src/app/takahashi/a00100/job/a00100/crawler/job/request/crawler/WebBrowser.java
@@ -1,6 +1,8 @@
 package app.takahashi.a00100.job.a00100.crawler.job.request.crawler;
 
 import java.io.File;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -12,6 +14,8 @@ import lombok.experimental.Accessors;
 @Accessors(prefix = "m_", chain = false)
 public class WebBrowser implements AutoCloseable {
 	static WebBrowser m_instance;
+	static Map<Long, _Current> m_currents = new ConcurrentHashMap<Long, _Current>();
+
 	_Current m_current;
 
 	WebBrowser() {
@@ -27,14 +31,20 @@ public class WebBrowser implements AutoCloseable {
 	}
 
 	public static _Current getCurrent() {
-		val x = getInstance();
-		return (x.m_current == null ? x.m_current = new _Current() : x.m_current);
+		val id = Thread.currentThread().getId();
+		_Current current = m_currents.get(id);
+
+		if (current == null) {
+			m_currents.put(id, current = new _Current());
+		}
+
+		return current;
 	}
 
 	@Override
 	public void close() throws Exception {
-		if (m_current != null) {
-			m_current.close();
+		for (val x : m_currents.values()) {
+			x.close();
 		}
 	}
 
