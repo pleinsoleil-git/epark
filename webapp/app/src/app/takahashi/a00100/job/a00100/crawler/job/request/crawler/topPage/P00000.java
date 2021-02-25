23149d2471fad45db6c315bef611b88ffbb25e49
NO001\hidek <hideki.nakayama@pleinsoleil.jp>
2021-02-26 08:34:09 +0900

    update

--- webapp/app/src/app/takahashi/a00100/job/a00100/crawler/job/request/crawler/topPage/P00000.java
+++ webapp/app/src/app/takahashi/a00100/job/a00100/crawler/job/request/crawler/topPage/P00000.java
@@ -2,6 +2,7 @@ package app.takahashi.a00100.job.a00100.crawler.job.request.crawler.topPage;
 
 import app.takahashi.a00100.job.a00100.crawler.job.request.Request;
 import app.takahashi.a00100.job.a00100.crawler.job.request.crawler.WebClient;
+import lombok.val;
 import lombok.experimental.Accessors;
 
 @Accessors(prefix = "m_", chain = false)
@@ -9,5 +10,11 @@ class P00000 extends WebClient {
 	String getURI() {
 		return "https://haisha-yoyaku.jp/bun2sdental/detail/index/id/"
 				+ Request.getCurrent().getCatalogId();
+	}
+
+	@Override
+	public void navigate() throws Exception {
+		val driver = getDriver();
+		driver.get(getURI());
 	}
 }
