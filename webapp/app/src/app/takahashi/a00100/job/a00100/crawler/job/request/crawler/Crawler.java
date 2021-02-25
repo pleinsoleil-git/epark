92aff67c4dae21f09ce5a532e5753bada3170cba
NO001\hidek <hideki.nakayama@pleinsoleil.jp>
2021-02-26 07:54:39 +0900

    update

--- webapp/app/src/app/takahashi/a00100/job/a00100/crawler/job/request/crawler/Crawler.java
+++ webapp/app/src/app/takahashi/a00100/job/a00100/crawler/job/request/crawler/Crawler.java
@@ -3,6 +3,7 @@ package app.takahashi.a00100.job.a00100.crawler.job.request.crawler;
 import java.util.ArrayList;
 import java.util.Collection;
 
+import app.takahashi.a00100.job.a00100.crawler.job.request.crawler.topPage.TopPage;
 import lombok.Data;
 import lombok.val;
 import lombok.experimental.Accessors;
@@ -45,9 +46,11 @@ public class Crawler {
 	@Data
 	public static class _Current {
 		public void execute() throws Exception {
+			topPage();
 		}
 
 		void topPage() throws Exception {
+			TopPage.getInstance().execute();
 		}
 
 		void reservePage() throws Exception {
