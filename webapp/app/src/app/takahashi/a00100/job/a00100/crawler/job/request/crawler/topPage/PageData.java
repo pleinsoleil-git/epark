61c8dbb4f2fcac7e490ecb698bba334780b69e2d
hidek <hidek@NT001>
2021-02-25 14:42:53 +0900

    update

---
+++ webapp/app/src/app/takahashi/a00100/job/a00100/crawler/job/request/crawler/topPage/PageData.java
@@ -0,0 +1,15 @@
+package app.takahashi.a00100.job.a00100.crawler.job.request.crawler.topPage;
+
+import app.takahashi.a00100.job.a00100.crawler.job.request.Result;
+
+class PageData extends Result {
+	static ThreadLocal<PageData> m_currents = new ThreadLocal<PageData>() {
+		@Override
+		protected PageData initialValue() {
+			return new PageData();
+		}
+	};
+
+	PageData() {
+	}
+}
