9a73c11ea35817595b842ed9323a0bd738fea9d8
hidek <hidek@NT001>
2021-02-25 14:14:20 +0900

    update

--- webapp/app/src/app/takahashi/a00100/job/a00100/crawler/job/request/crawler/CrawlerData.java
+++ webapp/app/src/app/takahashi/a00100/job/a00100/crawler/job/request/crawler/CrawlerData.java
@@ -1,7 +1,13 @@
 package app.takahashi.a00100.job.a00100.crawler.job.request.crawler;
 
 public class CrawlerData {
+	static CrawlerData m_instance;
+
 	CrawlerData() {
+	}
+
+	public static CrawlerData getInstance() {
+		return (m_instance == null ? m_instance = new CrawlerData() : m_instance);
 	}
 
 	public void save() throws Exception {
