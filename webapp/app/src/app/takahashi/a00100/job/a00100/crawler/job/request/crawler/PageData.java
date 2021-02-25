2d717b7166064453f4621170cc86ded1b5e67543
hidek <hidek@NT001>
2021-02-25 14:03:05 +0900

    update

--- webapp/app/src/app/takahashi/a00100/job/a00100/crawler/job/request/crawler/PageData.java
+++ webapp/app/src/app/takahashi/a00100/job/a00100/crawler/job/request/crawler/PageData.java
@@ -1,5 +1,9 @@
 package app.takahashi.a00100.job.a00100.crawler.job.request.crawler;
 
-public interface PageData {
-	public void save() throws Exception;
+public class PageData {
+	public void save() throws Exception {
+	}
+
+	public static class _Current {
+	}
 }
