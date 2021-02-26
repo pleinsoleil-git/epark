01e287bddb8af74ca320e291e255f75de1327f00
NO001\hidek <hideki.nakayama@pleinsoleil.jp>
2021-02-26 09:12:05 +0900

    update

--- webapp/app/src/app/takahashi/a00100/job/a00100/crawler/job/request/crawler/topPage/P00000.java
+++ webapp/app/src/app/takahashi/a00100/job/a00100/crawler/job/request/crawler/topPage/P00000.java
@@ -10,9 +10,12 @@ import org.openqa.selenium.interactions.Actions;
 import app.takahashi.a00100.job.a00100.crawler.job.request.Request;
 import app.takahashi.a00100.job.a00100.crawler.job.request.crawler.WebClient;
 import common.lang.StringUtils;
+import common.lang.exception.EmptyException;
 import lombok.val;
 import lombok.experimental.Accessors;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Accessors(prefix = "m_", chain = false)
 class P00000 extends WebClient {
 	String getURI() {
@@ -72,40 +75,55 @@ class P00000 extends WebClient {
 	void getMeniList() throws Exception {
 		val data = PageData.getCurrent();
 		val driver = getDriver();
-		val menuList = new LinkedHashMap<String, ArrayList<String>>() {
-			{
-				val by = By.xpath("//ul[@class='tab_detail-clinic ' or @class='tab_detail-clinic']");
 
-				for (val x : driver.findElements(by)) {
-					for (val menu : x.findElements(By.xpath("./child::li/a"))) {
-						val title = StringUtils.split(menu.getText())[0];
-						if (containsKey(title) == true) {
-							continue;
-						}
+		for (int i = 0; i < 10; i++) {
+			try {
+				val menuList = new LinkedHashMap<String, ArrayList<String>>() {
+					{
+						val by = By.xpath("//ul[@class='tab_detail-clinic ' or @class='tab_detail-clinic']");
 
-						put(title, new ArrayList<String>() {
-							{
-								// --------------------------------------------------
-								// ホバーして一度表示しないとテキストが取れない
-								// --------------------------------------------------
-								val items = menu.findElements(By.xpath("./following-sibling::ul/li/a"));
-								if (items.isEmpty() == false) {
-									val action = new Actions(driver);
-									action.moveToElement(menu).build().perform();
-
-									for (val item : items) {
-										action.clickAndHold(item).build().perform();
-										add(item.getText());
-									}
+						for (val x : driver.findElements(by)) {
+							for (val menu : x.findElements(By.xpath("./child::li/a"))) {
+								val title = StringUtils.split(menu.getText())[0];
+								if (containsKey(title) == true) {
+									continue;
 								}
-							}
-						});
-					}
-				}
-			}
-		};
 
-		data.setMenuList(menuList);
+								put(title, new ArrayList<String>() {
+									{
+										// --------------------------------------------------
+										// ホバーして一度表示しないとテキストが取れない
+										// --------------------------------------------------
+										val items = menu.findElements(By.xpath("./following-sibling::ul/li/a"));
+										if (items.isEmpty() == false) {
+											val action = new Actions(driver);
+											action.moveToElement(menu).build().perform();
+
+											for (val item : items) {
+												action.clickAndHold(item).build().perform();
+
+												val text = item.getText();
+												if (StringUtils.isEmpty(text) == true) {
+													throw new EmptyException();
+												}
+
+												add(text);
+											}
+										}
+									}
+								});
+							}
+						}
+					}
+				};
+
+				data.setMenuList(menuList);
+				return;
+			} catch (EmptyException e) {
+				log.error(String.format("Empty menu[id=%s]",
+						Request.getCurrent().getCatalogId()));
+			}
+		}
 	}
 
 	void getNetReserveType() throws Exception {
---
+++ webapp/app/src/common/lang/exception/EmptyException.java
@@ -0,0 +1,10 @@
+package common.lang.exception;
+
+public class EmptyException extends UserException {
+	public EmptyException() {
+	}
+
+	public EmptyException(final String message) {
+		super(message);
+	}
+}
