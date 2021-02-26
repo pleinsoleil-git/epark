8e6df22cf3c4700447a6bba69a16a56e96534dd6
NO001\hidek <hideki.nakayama@pleinsoleil.jp>
2021-02-26 09:55:53 +0900

    update

--- webapp/app/src/app/takahashi/a00100/job/a00100/crawler/job/request/crawler/Crawler.java
+++ webapp/app/src/app/takahashi/a00100/job/a00100/crawler/job/request/crawler/Crawler.java
@@ -3,12 +3,15 @@ package app.takahashi.a00100.job.a00100.crawler.job.request.crawler;
 import java.util.ArrayList;
 import java.util.Collection;
 
+import app.takahashi.a00100.job.a00100.crawler.job.request.Request;
 import app.takahashi.a00100.job.a00100.crawler.job.request.crawler.reserve.Reserve;
 import app.takahashi.a00100.job.a00100.crawler.job.request.crawler.top.Top;
 import lombok.Data;
 import lombok.val;
 import lombok.experimental.Accessors;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Accessors(prefix = "m_", chain = false)
 public class Crawler {
 	static ThreadLocal<Crawler> m_instances = new ThreadLocal<Crawler>() {
@@ -47,6 +50,9 @@ public class Crawler {
 	@Data
 	public static class _Current {
 		public void execute() throws Exception {
+			log.info(String.format("Crawler[id=%s]",
+					Request.getCurrent().getCatalogId()));
+
 			top();
 			reserve();
 		}
