7c4bad203542f24fd6b6efdb20ce247414d943af
NO001\hidek <hideki.nakayama@pleinsoleil.jp>
2021-03-02 08:30:38 +0900

    update

---
+++ webapp/app/sql/nakajo/a00100/i_usage_history.sql
@@ -0,0 +1,42 @@
+DROP TABLE i_usage_history CASCADE;
+
+
+CREATE UNLOGGED TABLE i_usage_history
+(
+	id								BIGSERIAL,
+	data_type						VARCHAR,	-- VOC：VOCデータ（メールアドレスのみ）：Silent
+	usage_history_id				VARCHAR,	-- id
+	media_id						VARCHAR,	-- media_id
+	service							VARCHAR,	-- サービス
+	usage_date						VARCHAR,	-- 日付
+	member_id						VARCHAR,	-- EPARK会員ID
+	evaluation						VARCHAR,	-- 評価
+	usage_type						VARCHAR,	-- 種別
+	reserve_1						VARCHAR,	-- 予備1
+	reserve_2						VARCHAR,	-- 予備2
+	usage_within_last_2_year		VARCHAR,	-- 過去2年以内利用回数
+	usage_within_last_1_year		VARCHAR,	-- 過去1年以内利用回数
+	usage_within_last_6_month		VARCHAR,	-- 過去6ヵ月利用回数
+	usage_within_after_30_day		VARCHAR,	-- その後30日以内利用回数
+	usage_within_after_60_day		VARCHAR,	-- その後60日以内利用回数
+	usage_within_after_90_day		VARCHAR,	-- その後90日以内利用回数
+	usage_within_after_120_day		VARCHAR,	-- その後120日以内利用回数
+	usage_within_after_150_day		VARCHAR,	-- その後150日以内利用回数
+	usage_within_after_180_day		VARCHAR,	-- その後180日以内利用回数
+	usage_within_after_1_year		VARCHAR,	-- その後1年以内利用
+	usage_within_after_2_year		VARCHAR,	-- その後2年以内利用
+	all_usage_within_last_6_month	VARCHAR,	-- （ALL）過去6ヵ月利用
+	all_usage_within_after_30_day	VARCHAR,	-- （ALL）その後30日以内利用
+	all_usage_within_after_60_day	VARCHAR,	-- （ALL）その後60日以内利用
+	all_usage_within_after_90_day	VARCHAR,	-- （ALL）その後90日以内利用
+	all_usage_within_after_120_day	VARCHAR,	-- （ALL）その後120日以内利用
+	all_usage_within_after_150_day	VARCHAR,	-- （ALL）その後150日以内利用
+	all_usage_within_after_180_day	VARCHAR,	-- （ALL）その後180日以内利用
+	created_at						TIMESTAMP( 0 )		DEFAULT CURRENT_TIMESTAMP
+)
+WITH
+(
+	FILLFACTOR = 100,
+	AUTOVACUUM_ENABLED = FALSE
+)
+TABLESPACE pg_default;
---
+++ webapp/app/sql/nakajo/a00100/t_usage_history.sql
@@ -0,0 +1,39 @@
+DROP TABLE t_usage_history CASCADE;
+
+
+CREATE TABLE t_usage_history
+(
+	id								BIGSERIAL,
+	data_type						VARCHAR( 512 ),	-- VOC：VOCデータ（メールアドレスのみ）：Silent
+	usage_history_id				NUMERIC,			-- id
+	media_id						NUMERIC,			-- media_id
+	service							VARCHAR( 512 ),	-- サービス
+	usage_date						DATE,				-- 日付
+	member_id						NUMERIC,			-- EPARK会員ID
+	evaluation						VARCHAR( 512 ),	-- 評価
+	usage_type						VARCHAR( 512 ),	-- 種別
+	usage_within_last_2_year		NUMERIC,			-- 過去2年以内利用回数
+	usage_within_last_1_year		NUMERIC,			-- 過去1年以内利用回数
+	usage_within_last_6_month		NUMERIC,			-- 過去6ヵ月利用回数
+	usage_within_after_30_day		NUMERIC,			-- その後30日以内利用回数
+	usage_within_after_60_day		NUMERIC,			-- その後60日以内利用回数
+	usage_within_after_90_day		NUMERIC,			-- その後90日以内利用回数
+	usage_within_after_120_day		NUMERIC,			-- その後120日以内利用回数
+	usage_within_after_150_day		NUMERIC,			-- その後150日以内利用回数
+	usage_within_after_180_day		NUMERIC,			-- その後180日以内利用回数
+	usage_within_after_1_year		NUMERIC,			-- その後1年以内利用
+	usage_within_after_2_year		NUMERIC,			-- その後2年以内利用
+	all_usage_within_last_6_month	NUMERIC,			-- （ALL）過去6ヵ月利用
+	all_usage_within_after_30_day	NUMERIC,			-- （ALL）その後30日以内利用
+	all_usage_within_after_60_day	NUMERIC,			-- （ALL）その後60日以内利用
+	all_usage_within_after_90_day	NUMERIC,			-- （ALL）その後90日以内利用
+	all_usage_within_after_120_day	NUMERIC,			-- （ALL）その後120日以内利用
+	all_usage_within_after_150_day	NUMERIC,			-- （ALL）その後150日以内利用
+	all_usage_within_after_180_day	NUMERIC,			-- （ALL）その後180日以内利用
+	created_at						TIMESTAMP( 0 )		DEFAULT CURRENT_TIMESTAMP
+)
+WITH
+(
+	FILLFACTOR = 100
+)
+TABLESPACE pg_default;
