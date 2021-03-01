21a3d6ef19c2791eeb15b85feec743e78eb108b8
NO001\hidek <hideki.nakayama@pleinsoleil.jp>
2021-03-01 11:53:29 +0900

    update

--- webapp/app/data/対象医院.sql
+++ webapp/app/data/対象医院.sql
@@ -1,12 +1,34 @@
-TRUNCATE TABLE j_request CASCADE;
+TRUNCATE TABLE i_clinic CASCADE;
 
 
-COPY j_request
+COPY i_clinic
 (
 	catalog_id
 )
 FROM 'D:/Temp/clinic.csv'
 
 
-UPDATE j_request
-SET foreign_id = 27;
+TRUNCATE TABLE j_request CASCADE;
+
+
+WITH s_params AS
+(
+	SELECT 27::BIGINT AS job_id
+)
+INSERT INTO j_request
+(
+	foreign_id,
+	catalog_id,
+	shopowner_id,
+	deleted
+)
+SELECT t10.job_id,
+	t20.catalog_id,
+	m10.shopowner_id,
+	TRUE
+FROM s_params AS t10
+CROSS JOIN i_clinic AS t20
+INNER JOIN m_clinic AS m10
+	ON m10.foreign_id = t10.job_id
+	AND m10.catalog_id = t20.catalog_id
+ORDER BY t20.id;
