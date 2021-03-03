ab6d7ca394bfbb1b87fdfdd1a7c4fe8cbe7cb6ad
NO001\hidek <hideki.nakayama@pleinsoleil.jp>
2021-03-03 10:53:03 +0900

    update

--- webapp/app/src/app/nakajo/a00100/job/a00100/export/job/request/report/Report.java
+++ webapp/app/src/app/nakajo/a00100/job/a00100/export/job/request/report/Report.java
@@ -1,12 +1,21 @@
 package app.nakajo.a00100.job.a00100.export.job.request.report;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Collection;
 
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+import app.nakajo.a00100.job.a00100.export.job.Job;
+import app.nakajo.a00100.job.a00100.export.job.request.report.member.Member;
+import common.poi.WorkbookUtils;
 import lombok.Data;
 import lombok.val;
 import lombok.experimental.Accessors;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Accessors(prefix = "m_", chain = false)
 public class Report {
 	static Report m_instance;
@@ -43,7 +52,33 @@ public class Report {
 
 	@Data
 	public static class _Current {
+		Workbook m_workbook;
+
+		public Workbook getWorkbook() {
+			if (m_workbook == null) {
+				try (val rs = getClass().getResourceAsStream("Report.xlsx")) {
+					m_workbook = WorkbookFactory.create(rs);
+				} catch (Exception e) {
+					log.error("", e);
+				}
+			}
+
+			return m_workbook;
+		}
+
 		public void execute() throws Exception {
+			output();
+		}
+
+		void output() throws Exception {
+			try (val book = getWorkbook()) {
+				member();
+				WorkbookUtils.save(book, new File(Job.getCurrent().getOutputFile()));
+			}
+		}
+
+		void member() throws Exception {
+			Member.getInstance().execute();
 		}
 	}
 }
