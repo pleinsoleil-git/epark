3bf4cd7f97984339f65ce6e507b4ceb6f05be2d7
0028883 <0028883@EPF20L0050.epark.co.jp>
2021-03-10 13:43:50 +0900

    a

--- webapp/app/sql/nakajo/a00100/tmp_repeat_report.sql
+++ webapp/app/sql/nakajo/a00100/tmp_repeat_report.sql
@@ -7,24 +7,20 @@ CREATE UNLOGGED TABLE tmp_repeat_report
 	usage_month								DATE,				-- 日付
 	evaluation								VARCHAR( 512 ),	-- 評価
 	channel									VARCHAR( 512 ),	-- 種別
-	usage_within_last_2_year				NUMERIC,			-- 過去2年以内利用回数件数
-	usage_within_last_1_year				NUMERIC,			-- 過去1年以内利用回数件数
-	usage_within_last_6_month				NUMERIC,			-- 過去6ヵ月利用回数件数
-	usage_within_after_30_day				NUMERIC,			-- その後30日以内利用回数件数
-	usage_within_after_60_day				NUMERIC,			-- その後60日以内利用回数件数
-	usage_within_after_90_day				NUMERIC,			-- その後90日以内利用回数件数
-	usage_within_after_120_day				NUMERIC,			-- その後120日以内利用回数件数
-	usage_within_after_150_day				NUMERIC,			-- その後150日以内利用回数件数
-	usage_within_after_180_day				NUMERIC,			-- その後180日以内利用回数件数
-	usage_within_after_1_year				NUMERIC,			-- その後1年以内利用件数
-	usage_within_after_2_year				NUMERIC,			-- その後2年以内利用件数
-	all_usage_within_last_6_month			NUMERIC,			-- （ALL）過去6ヵ月利用件数
-	all_usage_within_after_30_day			NUMERIC,			-- （ALL）その後30日以内利用件数
-	all_usage_within_after_60_day			NUMERIC,			-- （ALL）その後60日以内利用件数
-	all_usage_within_after_90_day			NUMERIC,			-- （ALL）その後90日以内利用件数
-	all_usage_within_after_120_day			NUMERIC,			-- （ALL）その後120日以内利用件数
-	all_usage_within_after_150_day			NUMERIC,			-- （ALL）その後150日以内利用件数
-	all_usage_within_after_180_day			NUMERIC,			-- （ALL）その後180日以内利用件数
+	last_6_month							NUMERIC,			-- 過去6ヵ月利用回数件数
+	after_30_day							NUMERIC,			-- その後30日以内利用回数件数
+	after_60_day							NUMERIC,			-- その後60日以内利用回数件数
+	after_90_day							NUMERIC,			-- その後90日以内利用回数件数
+	after_120_day							NUMERIC,			-- その後120日以内利用回数件数
+	after_150_day							NUMERIC,			-- その後150日以内利用回数件数
+	after_180_day							NUMERIC,			-- その後180日以内利用回数件数
+	all_last_6_month						NUMERIC,			-- （ALL）過去6ヵ月利用件数
+	all_after_30_day						NUMERIC,			-- （ALL）その後30日以内利用件数
+	all_after_60_day						NUMERIC,			-- （ALL）その後60日以内利用件数
+	all_after_90_day						NUMERIC,			-- （ALL）その後90日以内利用件数
+	all_after_120_day						NUMERIC,			-- （ALL）その後120日以内利用件数
+	all_after_150_day						NUMERIC,			-- （ALL）その後150日以内利用件数
+	all_after_180_day						NUMERIC,			-- （ALL）その後180日以内利用件数
 	created_at								TIMESTAMP( 0 )		DEFAULT CURRENT_TIMESTAMP
 )
 WITH
